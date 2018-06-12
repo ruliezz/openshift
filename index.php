183435599755bff5f7fe3736892d11d479273cfe
Ruben Felix <ruben.felix@sonepar.nl>
2018-06-12 16:21:35 +0200

    aanpassing

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php 
-echo date("Y-m-d");
-echo $_SERVER['SERVER_ADDR'];
+echo "<center><img src=\"smile.png\"></center>";
+
+echo "<h1>"; 
+echo date("Y-m-d"); 
+echo "</h1>";
+
+echo "<br/>";
+echo "<h1>"; 
+ echo $_SERVER['SERVER_ADDR']; 
+ echo "</h1>";
 ?>
