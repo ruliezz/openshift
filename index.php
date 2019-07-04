e8792eb7e8476c9b818b6014f515d7ac3c07c4c7
ruliezz <info@rubenfelix.nl>
2019-07-04 14:16:36 +0200

    Update index.php
    test

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
+
 <?php 
+
 echo "<center><img src=\"smile.jpg\"></center>";
 echo "<center><img src=\"images.jpeg\"></center>";
 echo "<center><img src=\"dubblesicon.jpg\"></center>";
@@ -6,7 +8,7 @@ echo "dikke test";
 echo "<h1>"; 
 echo date("Y-m-d"); 
 echo "</h1>";
-echo $name = getenv('myname');
+echo $name = getenv('name');
 echo "<br/>";
 echo "<h1>"; 
  echo $_SERVER['SERVER_ADDR']; 
