170b0fb261925f532052eb17823676708c5ec8d2
ruliezz <info@rubenfelix.nl>
2020-06-22 14:51:02 +0200

    test

--- index.php
+++ index.php
@@ -4,13 +4,7 @@
 echo "<center><img src=\"smile.jpg\"></center>";
 echo "<center><img src=\"images.jpeg\"></center>";
 echo "<center><img src=\"dubblesicon.jpg\"></center>";
-echo "dikke test";
 echo "<h1>"; 
-echo date("Y-m-d"); 
+echo "testen";
 echo "</h1>";
-echo $name = getenv('name');
-echo "<br/>";
-echo "<h1>"; 
- echo $_SERVER['SERVER_ADDR']; 
- echo "</h1>";
 ?>
