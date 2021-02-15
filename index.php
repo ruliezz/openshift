271332879c3e59105c6a8f7ebfae0504e6b6e990
ruliezz <info@rubenfelix.nl>
2021-02-15 10:11:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 
 <?php 
 
+if ($handle = opendir('media')) {
+    echo "Directory handle: $handle\n";
+    echo "Entries:\n";
+
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($handle))) {
+        echo "$entry\n";
+    }
+}
+
+
+
+echo "<hr />";
 echo "<center><img src=\"smile.jpg\"></center>";
 echo "<center><img src=\"images.jpeg\"></center>";
 echo "<center><img src=\"dubblesicon.jpg\"></center>";
